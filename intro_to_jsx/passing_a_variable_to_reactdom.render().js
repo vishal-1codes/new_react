acc53cf4bc067ec75ec2ad35edaad3899c15e90e
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2020-12-29 13:36:39 +0530

    Create passing_a_variable_to_reactdom.render().js

---
+++ intro_to_jsx/passing_a_variable_to_reactdom.render().js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//we create veriable myList
+const myList = (
+  <ul> 
+  <li>vvp</li>
+  <li>vp</li>
+  </ul>
+);
+//first Argument can be varibale
+ReactDOM.render(myList,document.getElementById('app'));
