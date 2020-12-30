838d5042d5fa7ee0e926f0b1f11ff56df55c555c
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2020-12-30 11:15:15 +0530

    Create self_closing_tag.js

---
+++ advanced_jsx/self_closing_tag.js
@@ -0,0 +1,14 @@
+const profile = (
+  <div>
+    <h1>I AM JENKINS</h1>
+    <img src="images/jenkins.png"/> 
+    <article>
+      I LIKE TO SIT
+      <br/>
+      JENKINS IS MY NAME
+      <br/>
+      THANKS HA LOT
+    </article>
+  </div>
+);
+//we clase <img/> & <br/> tag here
