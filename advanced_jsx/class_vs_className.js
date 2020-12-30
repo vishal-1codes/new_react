c9c6ca9fdb63a2ef31f18579228d0cec6651b21a
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2020-12-30 11:09:50 +0530

    Create class_vs_className.js

---
+++ advanced_jsx/class_vs_className.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//new variable declare with className 
+const myDiv = (<div className="big">I AM A BIG DIV </div>);
+//seperate line can be form 
+ReactDOM.render(myDiv,document.getElementById('app'));
