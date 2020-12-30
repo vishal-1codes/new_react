a7537c876b2825167d275b32364012de81094673
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2020-12-30 11:36:12 +0530

    Create veriable_jsx.js

---
+++ advanced_jsx/veriable_jsx.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//creating veriable
+const theBestString = 'tralalalala i am da best';
+//we print veriable store value
+ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
