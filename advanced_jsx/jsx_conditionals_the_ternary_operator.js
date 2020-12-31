c288bc9b5f9e2c965c9404da244ffc86320d5ca6
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2020-12-31 12:56:35 +0530

    Create jsx_conditionals_the_ternary_operator.js

---
+++ advanced_jsx/jsx_conditionals_the_ternary_operator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+//here code of ternary operator
+const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
+ReactDOM.render(
+	img, 
+	document.getElementById('app')
+);
