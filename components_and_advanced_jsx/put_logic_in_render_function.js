d3c11f02740e8c4a1d65bed46018fb2b2a763674
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-05 11:36:38 +0530

    Create put_logic_in_render_function.js

---
+++ components_and_advanced_jsx/put_logic_in_render_function.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys!  Wait Up!",
+    src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg"
+  }
+];
+// New component class
+class Friend extends React.Component {
+  render() {
+    let friend = friends[0];
+  	return (
+    	<div>
+        <h1>{friend.title}</h1>
+        <img src={friend.src} />
+    	</div>
+  	);
+	}
+}
+ReactDOM.render(<Friend />, document.getElementById('app'));
