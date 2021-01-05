16c3d3af8d55347bcc79dd24f0d5c6b3c8ce1a25
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-05 16:43:16 +0530

    Create render_a_components_props.js

---
+++ this.props/render_a_components_props.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hi there, {this.props.firstName}!</h1>;
+  }
+}
+ReactDOM.render(
+  <Greeting firstName='Rybu' />, 
+  document.getElementById('app')
+);
