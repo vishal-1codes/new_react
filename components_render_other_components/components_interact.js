0ddf0dfed25401e8aff8145c60941afa3b97d30b
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-05 13:49:49 +0530

    Create components_interact.js

---
+++ components_render_other_components/components_interact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <h1>All About Me!</h1>
+        <p>I like movies and blah blah blah blah blah</p>
+        <img src="https://content.codecademy.com/courses/React/react_photo-monkeyselfie.jpg" />
+      </div>
+    );
+  }
+}
