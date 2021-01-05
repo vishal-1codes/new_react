366a65702688920de2ebae693b0faa16f3366386
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-05 17:39:27 +0530

    Create receive_an_event_handler_as_a_prop.js

---
+++ this.props/receive_an_event_handler_as_a_prop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export class Button extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.talk} >
+        Click me!
+      </button>
+    );
+  }
+}
