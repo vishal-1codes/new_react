145e9fa0d02e4d19bad45870cc2e834cbccb992d
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-05 16:36:13 +0530

    Create pass_props_to_a_component.js

---
+++ this.props/pass_props_to_a_component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+};
+ReactDOM.render(<PropsDisplayer myProp="Hello"/>, document.getElementById('app'));
