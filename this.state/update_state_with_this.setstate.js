77a3f2b86653cae9ebaa400c7c7323ad16dd9253
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-06 12:20:32 +0530

    Create update_state_with_this.setstate.js

---
+++ this.state/update_state_with_this.setstate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class Example extends React.Component {
+  constructor(props) {
+  	super(props);
+    this.state = {
+      mood:   'great',
+      hungry: false
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+<Example />
