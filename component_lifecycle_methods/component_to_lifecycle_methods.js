0f5c9145e9d2862cb926bf2f94d276b29f6acb71
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-06 17:03:11 +0530

    Create component_to_lifecycle_methods.js

---
+++ component_lifecycle_methods/component_to_lifecycle_methods.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+  componentDidMount() {
+    const oneSecond = 1000;
+setInterval(() => {
+  this.setState({ date: new Date() });
+}, oneSecond);
+  }
+}
+ReactDOM.render(<Clock />, document.getElementById('app'));
