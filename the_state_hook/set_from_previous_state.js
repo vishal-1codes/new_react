47fa946100b724d0ea129e7a5f2b3fa640b10cae
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-07 18:39:29 +0530

    Create set_from_previous_state.js

---
+++ the_state_hook/set_from_previous_state.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+export default function QuizNavBar({ questions }) {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const goBack = () =>
+    setQuestionIndex((prevQuestionIndex) => prevQuestionIndex - 1);
+  const goToNext = () =>
+    setQuestionIndex((prevQuestionIndex) => prevQuestionIndex + 1);
+  const onFirstQuestion = questionIndex === 0;
+  const onLastQuestion = questionIndex === questions.length - 1;
+  return (
+    <nav>
+      <span>Question #{questionIndex + 1}</span>
+      <div>
+        <button onClick={goBack} disabled={onFirstQuestion}>
+          Go Back
+        </button>
+        <button onClick={goToNext} disabled={onLastQuestion}>
+          Next Question
+        </button>
+      </div>
+    </nav>
+  );
+}
