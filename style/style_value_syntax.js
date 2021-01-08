d48bb3224f76af1a48946f47bbe3e30084d65a04
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-08 17:40:14 +0530

    Create style_value_syntax.js

---
+++ style/style_value_syntax.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const styles = {
+  background: 'lightblue',
+  color:      'darkred',
+  marginTop: 100,
+  fontSize: 50
+};
+//px can be remove from 100  & 50
+const styleMe = <h1 style={styles}>Please style me! I am so bland!</h1>;
+ReactDOM.render(
+	styleMe, 
+	document.getElementById('app')
+);
