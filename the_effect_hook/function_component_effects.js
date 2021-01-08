98ee3432abaf6b27fe07e7a2858ce9590fd3665a
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-08 11:12:41 +0530

    Create function_component_effects.js

---
+++ the_effect_hook/function_component_effects.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+//import useEffect then call useEffect then count response
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    alert(`Count: ${count}`);
+  });
+  const handleClick = () => {
+    setCount((prevCount) =>  prevCount + 1);
+  };
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>
+        Click me
+      </button>
+    </div>
+  );
+}
