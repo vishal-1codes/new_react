fa6da193350b11ffacabbe70cd36a0320ed05464
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-08 16:06:27 +0530

    Create don'n_update_props.js

---
+++ stateless_component_from_stateful_components/don'n_update_props.js
@@ -0,0 +1,7 @@
+import React from 'react';
+class Bad extends React.Component {
+  render() {
+    this.props.message = 'yo'; // NOOOOOOOOOOOOOO!!!
+    return <h1>{this.props.message}</h1>;
+  }
+}
