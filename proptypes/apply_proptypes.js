cb3bc1c79e5df66e83dbc2b8efcd56cc528e4a28
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-09 17:40:15 +0530

    Create apply_proptypes.js

---
+++ proptypes/apply_proptypes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import propTypes
+export class BestSeller extends React.Component {
+  render() {
+    return (
+      <li>
+        Title: <span>
+          {this.props.title}
+        </span><br />
+        
+        Author: <span>
+          {this.props.author}
+        </span><br />
+        
+        Weeks: <span>
+          {this.props.weeksOnList}
+        </span>
+      </li>
+    );
+  }
+}
+//create component class 
+BestSeller.propTypes={
+
+}
