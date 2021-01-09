8476d9048fd73adea3d88506abfee422fe64a09c
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-09 17:57:12 +0530

    Create proptypes_in_function_components.js

---
+++ proptypes/proptypes_in_function_components.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+export const GuineaPigs = (props) => {
+  let src = props.src;
+  return (
+    <div>
+      <h1>Cute Guinea Pigs</h1>
+      <img src={src} />
+    </div>
+  );
+}
+//we declare proptypes
+GuineaPigs.propTypes={
+src: PropTypes.string.isRequired
+};
