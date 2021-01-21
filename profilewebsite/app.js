c80f6d9dba234f8fae0990af056a24181c734f97
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-21 20:43:49 +0530

    Create app.js

---
+++ profilewebsite/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header';
+import SiteInfo from './components/main';
+import Footer from './components/footer';
+import './css/main.css';
+function App() {
+  return (
+   <div className="body">
+
+   <Header/>
+   <SiteInfo/>
+   <Footer/>
+   </div>
+
+  );
+}
