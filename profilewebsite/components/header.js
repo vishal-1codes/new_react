1ce0598739fa80c7ccf0f47521cb91e9bee9c4ba
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-21 20:41:32 +0530

    Create header.js

---
+++ profilewebsite/components/header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Header = () => {
+
+    return(
+        <div >
+        <nav>
+        <ul>
+         <li>
+             <a href="#">Home</a>
+         </li>
+         <li>
+             <a href="#">About</a>
+         </li>
+         <li>
+             <a href="#">Blog</a>
+         </li>
+         <li>
+             <a href="#">Contact</a>
+         </li>
+        </ul>
+        </nav>
+        </div>
+    );
+   
+
+}
+
+export default Header;
