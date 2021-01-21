819f63b031aa2d1bb36b1d3f049f29bfba1bd1f2
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-01-21 20:42:21 +0530

    Create footer.js

---
+++ profilewebsite/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+            <div>
+                <div className="social">
+                <i className="fab fa-twitter"></i>
+                <i className="fab fa-instagram"></i>
+                <i className="fab fa-youtube-square"></i>
+                <i className="fab fa-facebook-f"></i>
+                </div>
+                
+                <div className="copyright">
+                Copyright &copy; 2020. UI Syntax
+                </div>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
