225f817b7b8d2991056cbb1fc5edb7ab1502f1c3
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-03-11 21:29:29 +0530

    Create pure_java.js

---
+++ react_tutorial/day5/pure_java.js
@@ -0,0 +1,8 @@
+//h1 heading & p paragraph using pure java
+var h1= document.createElement("h1");
+h1.innerHTML="vishal pawar";
+document.getElementById("root").appendChild(h1);
+
+var p=document.createElement("p");
+p.innerHTML="hi  this is pure js";
+document.getElementById("root").appendChild(p);
