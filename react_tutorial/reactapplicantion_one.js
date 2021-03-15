a74229485617b996895018736e6ba58ebf2abf6e
vishal-1codes <74867381+vishal-1codes@users.noreply.github.com>
2021-03-15 21:08:26 +0530

    Create reactapplicantion_one.js

---
+++ react_tutorial/reactapplicantion_one.js
@@ -0,0 +1,16 @@
+/* creating simple react app for showing my favorite  aa filems using heading , paragraph , orderlist */
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <>
+    <h1>Vishal Pawar Favorite AA Films</h1>
+    <p>here TOP 3 Movies Of AA</p>
+    <ol>
+        <li>DANGEORUS KHILADI 5</li>
+        <li>MR PERFECT</li>
+        <li>AARYA</li>
+    </ol>
+    </>
+,document.getElementById("root")
+);
